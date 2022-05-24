64a93ad97b873c0247b6b663cbaf3c49d358713d
pegasusapp <oscar.2001601@gmail.com>
2022-05-23 21:07:54 -0500

    add crate charge bill

--- vistas/js/solicitudes.js
+++ vistas/js/solicitudes.js
@@ -11,35 +11,3 @@
 
      $( "#target_view" ).submit(); 
    }
-
-/*
-$(".btnDescItemSolicitud").click(function(){
-
-    var idSolicitud = $(this).attr("idSolicitud");
-    var datos = new FormData();
-    datos.append("idSolicitud", idSolicitud);
-
-    $.ajax({
-        url: "ajax/transformador.ajax.php",
-        method: "POST",
-        data: datos,
-        cache: false,
-        contentType: false,
-        processData: false,
-        dataType:"json",
-        success: function(respuesta){
-
-            $("#capacidad_nominalE").val(respuesta["capacidad_nominal"]);
-            $("#codigo_transformadorE").val(respuesta["codigo_transformador"]);
-            $("#longitudE").val(respuesta["longitud"]);
-            $("#laltitudE").val(respuesta["laltitud"]);
-            $("#altitudE").val(respuesta["altitud"]);
-            $("#voltaje_nominalE").val(respuesta["voltaje_nominal"]);
-            $("#dispo_potenciaE").val(respuesta["dispo_potencia"]);
-            $("#dispo_energiaE").val(respuesta["dispo_energia"]);
-        }
-
-    })
-
-
-}) */
