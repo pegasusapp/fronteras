ba1e86d89588920586c2ac44cdf16b2649b19a9a
pegasusapp <oscar.2001601@gmail.com>
2022-05-28 17:43:21 -0500

    cambios de cargar factura se añade el controlador a la fachada

--- controladores/validaciones.controlador.php
+++ controladores/validaciones.controlador.php
@@ -8,10 +8,10 @@ class ControladorValidaciones
 	Validar tipo de archivo pdf
 	=============================================*/
  
-	static public function validateFile($nameFile,$fileType)
+	static public function validateFile($file,$fileType)
 	{
 
-		if (mime_content_type($_FILES[$nameFile]['tmp_name']) == $fileType){
+		if (mime_content_type($file) == $fileType){
 			return true;
 		}
 		return false;
