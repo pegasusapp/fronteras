3f8d0027e6f0c51021d0e734676e8f6486ceb52d
pegasusapp <oscar.2001601@gmail.com>
2022-06-18 12:01:33 -0500

    add desviacion

--- vistas/js/desviacion.js
+++ vistas/js/desviacion.js
@@ -4,6 +4,6 @@ function editDataDesviacion(idlectura){
 	let myData = {"id": idlectura};
 
 
-			ajaxProcessEdit(myData,"ajax/editDataDesviacion.ajax.php","desviacion");
+			ajaxProcessEdit(myData,"ajax/desviacion.ajax.php","desviacion");
 	 
 }
