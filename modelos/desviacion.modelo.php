1917f2a05084fa8a39b2c9cc0adc3c65184182d1
pegasusapp <oscar.2001601@gmail.com>
2022-06-18 13:55:09 -0500

    add desviacion

--- modelos/desviacion.modelo.php
+++ modelos/desviacion.modelo.php
@@ -11,8 +11,7 @@ class ModeloDesviacion{
 			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla  WHERE $item = :$item");
 			$stmt -> bindParam(":".$item, $valor, PDO::PARAM_STR);
 			$stmt -> execute();
-			print_r($stmt -> fetch());
-			return $stmt -> fetch();
+			return $stmt -> fetchAll(PDO::FETCH_ASSOC);
 		}else{
 			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
 			$stmt -> execute();
