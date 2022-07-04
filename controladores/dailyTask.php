2daf194483d36a5265df7f52e87417b9fea14656
pegasusapp <oscar.2001601@gmail.com>
2022-07-04 15:26:47 -0500

    add functions to insert lecturas

--- controladores/dailyTask.php
+++ controladores/dailyTask.php
@@ -3,8 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require 'logLecturaWS.controlador.php';
-require dirname(__FILE__).'/modelos/logLecturaWS.modelo.php';
-require dirname(__FILE__).'/modelos/fronteras.modelo.php';
+require dirname(__FILE__)."/modelos/logLecturaWS.modelo.php";
+require dirname(__FILE__)."/modelos/fronteras.modelo.php";
 require 'utilidades.controlador.php';
 require 'constantes.controlador.php';
 
