ce6821bf4396ca65271d65b4e8845da2b09dfa93
pegasusapp <oscar.2001601@gmail.com>
2022-07-03 20:14:41 -0500

    add functions to insert lecturas

--- controladores/test.php
+++ controladores/test.php
@@ -1,6 +1,7 @@
 <?php
 
-require_once "../controladores/fronteras.controlador.php";
+require_once "fronteras.controlador.php";
 require_once "../modelos/fronteras.modelo.php";
+require_once "utilidades.controlador.php";
 
 ControladorFronteras::ctrPrepareDataToSendWS();
