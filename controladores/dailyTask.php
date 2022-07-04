a4fffabda94d4c7c74980a9319e9c7c8afa5e4d7
pegasusapp <oscar.2001601@gmail.com>
2022-07-04 13:04:45 -0500

    add functions to insert lecturas

--- controladores/dailyTask.php
+++ controladores/dailyTask.php
@@ -6,11 +6,43 @@ require $_SERVER['DOCUMENT_ROOT']."/modelos/fronteras.modelo.php";
 require $_SERVER['DOCUMENT_ROOT']."/controladores/utilidades.controlador.php";
 require $_SERVER['DOCUMENT_ROOT']."/controladores/constantes.controlador.php";
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
+
 class ControladorFronterasWS{ 
 
      public function ctrPrepareDataToSendWS(){
 	 
         $fecha = explode("-",ControladorUtilidades::anyoMesDia(1));	
+
+
+        	// La cuenta está bloqueada.
+            $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+            try {
+                    $mail->isSMTP();                                      // Set mailer to use SMTP
+                    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+                    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+                    $mail->Username = "notificaciones.italener@gmail.com";                 // SMTP username
+                    $mail->Password = "zqcjnrsntxejfuuk";                           // SMTP password
+                    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+                    $mail->Port = 587;                                     // TCP port to connect to
+                    $mail->From = "notificaciones.italener@gmail.com";
+                    $mail->FromName = "Italener";
+                    $mail->addAddress("oscar.2001601@gmail.com", 'Usuario :');     // Add a recipient
+                    $mail->isHTML(true);                                                      // Set email format to HTML
+                    $mail->Subject = "Hola";
+                    $mail->Body    = "Hola ".$fecha;
+                    $mail->send();
+                    
+                } 
+            catch (Exception $e)
+                {
+                        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+                }
+       
         $fronteras = ModeloFronteras::mdlMostrarFronteras("frontera",$item, $valor);
         foreach ($fronteras as $value){
                $array = self::ctrConexionLecturasFronteraWS($fecha[0],$fecha[1],$fecha[2],$value["fronteraCliente"]);
