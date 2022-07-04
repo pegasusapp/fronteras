e35907c5539a3cf33517712c476a59760b344098
pegasusapp <oscar.2001601@gmail.com>
2022-07-04 18:46:51 -0500

    add functions to insert lecturas

--- controladores/utilidades.controlador.php
+++ controladores/utilidades.controlador.php
@@ -1,9 +1,9 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-require 'PHPMailer-master/src/Exception.php';
-require 'PHPMailer-master/src/PHPMailer.php';
-require 'PHPMailer-master/src/SMTP.php';
+require_once 'PHPMailer-master/src/Exception.php';
+require_once 'PHPMailer-master/src/PHPMailer.php';
+require_once 'PHPMailer-master/src/SMTP.php';
 class ControladorUtilidades
 {
 
