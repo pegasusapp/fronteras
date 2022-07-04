9dac4530f9cefb1476559845563c4180894d3174
pegasusapp <oscar.2001601@gmail.com>
2022-07-04 16:00:42 -0500

    add functions to insert lecturas

--- controladores/constantes.controlador.php
+++ controladores/constantes.controlador.php
@@ -6,7 +6,7 @@ class Constantes{
 	const FILE_SIZE = "5000000"; // 5 Megas
 	const FROM_NAME_EMAIL = "Italener";
 	const EMAIL_COMPANY = "notificaciones.italener@gmail.com";
-	const PASS_EMAIL_COMPANY = "Od2001601.";
+	const PASS_EMAIL_COMPANY = "hzineevnwamzqldx";
 	const MSG_BLOQUEO = '<br><div class="alert alert-danger">Usted ha sido bloqueado</div>';
 	const MSG_INACTIVO = '<br><div class="alert alert-danger">Su usuario se encuentra inactivo</div>';
 	const MGS_ERROR_INGRESO = '<br><div class="alert alert-danger">Error al ingresar, vuelve a intentarlo</div>';
