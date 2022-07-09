ade7928584ef4e36f04f4e8bb1d6b0bb0f46ea33
pegasusapp <oscar.2001601@gmail.com>
2022-07-09 10:32:56 -0500

    change header and breadcrum

--- vistas/modulos/body.php
+++ vistas/modulos/body.php
@@ -1,17 +1,5 @@
 <?php 
-   $plantillasActivas = array("inicio" =>"inicio.php",
-                              "crearUsuario" => "crearUsuario.php",
-                              "editMenu" => "editMenu.php",
-                              "listadoFronteras" => "listadoFronteras.php",
-                              "datosPersonales" => "datosPersonales.php",
-                              "datosGenerados" => "datosGenerados.php",
-                              "listadoClientes" => "listadoClientes",
-                              "uploadFile" => "uploadFile.php",
-                              "uploadConsumo" => "uploadConsumo.php",
-                              "descargaFactura" => "descargaFactura.php",
-                              "desviacion" => "desviacion.php",
-                              "salir" => "salir.php"
-                                );
+
   if(isset($_SESSION["iniciarSesion"]) && $_SESSION["iniciarSesion"] == "ok"):?>
     <body class="hold-transition sidebar-mini layout-fixed">
       <div class="wrapper">
@@ -19,14 +7,10 @@
           include "cabezote.php";
           include "menu.php"; 
            if(isset($_GET["ruta"])){
-               if(isset($plantillasActivas[$_GET["ruta"]])){
-                      include $_GET["ruta"].".php";
-                    }
-                else{
-                      include "404.php";
-                    }
-          }else{
-            include "inicio.php";
+                include $template;
+              }
+            else{
+                include "inicio.php";
           }
          include "footer.php"; ?>
       </div>
