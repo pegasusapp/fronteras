5ad53cdf71724807d78f81f589cd995cadf4d9da
pegasusapp <oscar.2001601@gmail.com>
2022-07-12 21:44:12 -0500

    add factor m

--- modelos/factorm.modelo.php
+++ modelos/factorm.modelo.php
@@ -19,13 +19,38 @@ class ModeloFactorM{
 		}
 	}
 
+
+	static public function mdlSearchData($dataIn):int{
+        
+		$stmt = Conexion::conectar()->prepare("SELECT count(*) FROM factorm WHERE fecha = :fecha AND tipoEnergia = :tipoEnergia  AND frontera_fronteraCliente = :frontera_fronteraCliente");
+		$stmt -> bindParam(":fecha", $dataIn["fecha"], PDO::PARAM_STR);
+		$stmt -> bindParam(":frontera_fronteraCliente", $dataIn["frontera_fronteraCliente"], PDO::PARAM_STR);
+		$stmt -> bindParam(":tipoEnergia", $dataIn["tipoEnergia"], PDO::PARAM_STR);
+		$stmt -> execute();
+		return  $stmt->fetchColumn();
+	}
+
 	static public function mdlIngresarFactorM($tabla,$datosLog): bool{
-		$stmt =  Conexion::conectar()->prepare("INSERT INTO $tabla(fecha, tipoEnergia, cantidad, frontera_fronteraCliente) VALUES ( :fecha, :tipoEnergia, :cantidad, :frontera_fronteraCliente)");
-		$stmt->bindParam(":fecha", $datosLog["fecha"], PDO::PARAM_STR);
-		$stmt->bindParam(":tipoEnergia", $datosLog["tipoEnergia"], PDO::PARAM_STR);
-		$stmt->bindParam(":cantidad", $datosLog["cantidad"], PDO::PARAM_INT);
-		$stmt->bindParam(":frontera_fronteraCliente", $datosLog["frontera_fronteraCliente"], PDO::PARAM_STR);
-		return $stmt->execute();
+		
+		$valores = array("fecha" =>$datosLog["fecha"], 
+					  "tipoEnergia"=>$datosLog["tipoEnergia"],
+					  "frontera_fronteraCliente"=>$datosLog["frontera_fronteraCliente"]);
+		if(self::mdlSearchData($valores) == 0)
+		{
+			$stmt =  Conexion::conectar()->prepare("INSERT INTO $tabla(fecha, tipoEnergia, cantidad, frontera_fronteraCliente) VALUES ( :fecha, :tipoEnergia, :cantidad, :frontera_fronteraCliente)");
+			
+		}
+		else{
+			$stmt =  Conexion::conectar()->prepare("UPDATE $tabla SET  cantidad = :cantidad WHERE fecha = :fecha AND tipoEnergia = :tipoEnergia  AND frontera_fronteraCliente = :frontera_fronteraCliente");
+		}	
+	     	$stmt->bindParam(":fecha", $datosLog["fecha"], PDO::PARAM_STR);
+			$stmt->bindParam(":tipoEnergia", $datosLog["tipoEnergia"], PDO::PARAM_STR);
+			$stmt->bindParam(":cantidad", $datosLog["cantidad"], PDO::PARAM_INT);
+			$stmt->bindParam(":frontera_fronteraCliente", $datosLog["frontera_fronteraCliente"], PDO::PARAM_STR);
+			return $stmt->execute();		  
+		
+
+
 		
 	}
 
