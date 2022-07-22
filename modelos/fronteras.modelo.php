528b4378ce58fc26d460785b9fff3682995a1f2d
pegasusapp <oscar.2001601@gmail.com>
2022-07-21 22:55:24 -0500

    solve problem in frontera controlador

--- modelos/fronteras.modelo.php
+++ modelos/fronteras.modelo.php
@@ -12,28 +12,16 @@ class ModeloFronteras{
 	static public function mdlMostrarFronteras($tabla, $item, $valor)
 	{
 
-		$pdo = Conexion::conectar();
-		$pdo ->beginTransaction();
-		try 
-		{
-			if($item != null)
-			{
-				$stmt = $pdo->prepare("SELECT * FROM $tabla WHERE $item = :$item");
-				$stmt ->bindParam(":".$item, $valor, PDO::PARAM_STR);
-			}
-			else
-			{
-				$stmt = $pdo->prepare("SELECT  * FROM $tabla");
-			}
-			$stmt ->execute();
-			$pdo->commit();
+		if($item != null){
+			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla  WHERE $item = :$item");
+			$stmt -> bindParam(":".$item, $valor, PDO::PARAM_STR);
+			$stmt -> execute();
+			return $stmt -> fetch();
+		}else{
+			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
+			$stmt -> execute();
+			return $stmt -> fetchAll();
 		}
-		catch (PDOException $ex) 
-		{
-				$pdo->rollBack();
-				return $ex->getMessage();
-		}
-		return $stmt -> fetchAll(PDO::FETCH_ASSOC);
 	}
 
 	static public function mdlMostrarEnergiasFronteraDia($valor,$anyo_curso,$mes_curso,$dia_curso)
