5c46344d09e9434214b6e9655cbd739db6bb3a3f
pegasusapp <oscar.2001601@gmail.com>
2022-07-24 00:24:09 -0500

    optimizar plantilla inicio

--- modelos/fronteras.modelo.php
+++ modelos/fronteras.modelo.php
@@ -12,16 +12,28 @@ class ModeloFronteras{
 	static public function mdlMostrarFronteras($tabla, $item, $valor)
 	{
 
-		if($item != null){
-			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla  WHERE $item = :$item");
-			$stmt -> bindParam(":".$item, $valor, PDO::PARAM_STR);
-			$stmt -> execute();
-			return $stmt -> fetch();
-		}else{
-			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla");
-			$stmt -> execute();
-			return $stmt -> fetchAll();
+		$pdo = Conexion::conectar();
+		$pdo ->beginTransaction();
+		try 
+		{
+			if($item != null)
+			{
+				$stmt = $pdo->prepare("SELECT * FROM $tabla WHERE $item = :$item");
+				$stmt ->bindParam(":".$item, $valor, PDO::PARAM_STR);
+			}
+			else
+			{
+				$stmt = $pdo->prepare("SELECT  * FROM $tabla");
+			}
+			$stmt ->execute();
+			$pdo->commit();
 		}
+		catch (PDOException $ex) 
+		{
+				$pdo->rollBack();
+				return $ex->getMessage();
+		}		
+				return $stmt -> fetchAll(PDO::FETCH_ASSOC);
 	}
 
 	static public function mdlMostrarEnergiasFronteraDia($valor,$anyo_curso,$mes_curso,$dia_curso)
